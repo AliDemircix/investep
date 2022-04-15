638a9af8d738aaa94c6502034e0f611f8d188077
AliDemircix <alidemircix@gmail.com>
2022-04-15 23:50:36 +0200

    canto font added

--- src/app.js
+++ src/app.js
@@ -1,23 +1,25 @@
 let y = 0;
+var ratio = window.devicePixelRatio || 1;
 window.addEventListener('scroll', () => {
   y = window.pageYOffset;
   console.log(y);
-  if (y >= (window.innerWidth < 600 ? 900 : 650)) {
+  console.log(screen.width * ratio);
+  if (y >= (screen.width < 600 ? 900 : 650)) {
     document.querySelector('#slogan-1').classList.add('animate__fadeInRight');
   }
-  if (y >= (window.innerWidth < 600 ? 1050 : 660)) {
+  if (y >= (screen.width < 600 ? 1050 : 660)) {
     document.querySelector('#slogan-2').classList.add('animate__fadeInLeft');
   }
-  if (y >= (window.innerWidth < 600 ? 1300 : 670)) {
+  if (y >= (screen.width < 600 ? 1300 : 670)) {
     document.querySelector('#slogan-3').classList.add('animate__fadeInRight');
   }
-  if (y >= (window.innerWidth < 600 ? 1750 : 900)) {
+  if (y >= (screen.width < 600 ? 1750 : 900)) {
     document.querySelector('#slogan-4').classList.add('animate__fadeInRight');
   }
-  if (y >= (window.innerWidth < 600 ? 1950 : 900)) {
+  if (y >= (screen.width < 600 ? 1950 : 900)) {
     document.querySelector('#slogan-5').classList.add('animate__fadeInLeft');
   }
-  if (y >= (window.innerWidth < 600 ? 2250 : 900)) {
+  if (y >= (screen.width < 600 ? 2250 : 900)) {
     document.querySelector('#slogan-6').classList.add('animate__fadeInRight');
   }
 });
